8db0f055868d3250d6f91d27b9c2ffea763ff4b3
jandersen770 <jandersen770@gmail.com>
2021-03-16 12:17:32 -0400

    Clean up

--- src/page.jsx
+++ src/page.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { INCREMENT, DECREMENT } from './usePagesArrayReducer'
 
 export default function Page({ position, setPosition, pageNum, defaultZ, setDefaultZ }) {
 
--- src/session_book.jsx
+++ src/session_book.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState } from 'react';
 import Cover from './cover';
 import Page from './page';
 import BackCover from './back_cover';
-import { pagesArrayReducer } from './usePagesArrayReducer';
 
 export default function SessionBook() {
 
@@ -10,19 +9,12 @@ export default function SessionBook() {
     const [position, setPosition] = useState(0)
     const [bookClosed, setBookClosed] = useState(false)
     const [defaultZ, setDefaultZ] = useState(0)
-    // const [pagesArray, setPagesArray] = useState([...Array(5)].map((_, i) => <Page position={position} setPosition={setPosition} pageNum={6 - i} secondPageNum={i + 1} />))
-    
-    // const [state, dispatch] = useReducer(pagesArrayReducer, [...Array(5)].map((_, i) => <Page position={position} setPosition={setPosition} pageNum={6 - i} secondPageNum={i + 1} />));
-
-    useEffect(() => console.log(position))
 
     const makePages = () => {
         const pages = []
-        // pages.unshift()
         for (let i = 4; i < 10; i += 2) {
             pages.unshift(<Page position={position} setPosition={setPosition} pageNum={i} defaultZ={defaultZ} setDefaultZ={setDefaultZ} />)
         }
-        // pages.unshift()
         return pages
     }
 
